57e5f0801d3d8a50a73ae00f99da6448e51490b6
N-Ali <nausha@hotmail.fr>
2019-11-27 12:52:15 +0100

    Unity

---
+++ Assets/Scripts/Open World/Weather.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weather : MonoBehaviour
+{
+    public GameObject Player;
+    public GameObject Clouds;
+    public GameObject Sun;
+    private Light SunLight;
+
+    public float RotationSunSpeed = 1f;
+    public float CloudsSpeed = 1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SunLight = Sun.GetComponent<Light>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Clouds.transform.position.z < -1000)
+        {
+            Clouds.transform.position = new Vector3(Clouds.transform.position.x, Clouds.transform.position.y, 1000);
+        }
+        Clouds.transform.position = new Vector3(Clouds.transform.position.x, Clouds.transform.position.y, Clouds.transform.position.z - CloudsSpeed * Time.deltaTime);
+
+        SunLight.transform.Rotate(Vector3.right * RotationSunSpeed * Time.deltaTime);
+
+        if(Player.transform.position.y < -20)
+        {
+            Player.transform.position = new Vector3(482.9173f, 1.427047f, 207.8161f);
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerMouvement.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class PlayerMouvement : MonoBehaviour
+{
+    public float speed = 10.0f;
+    public float rotationSpeed = 100.0f;
+    public GameObject camera; 
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float translationVertical = Input.GetAxis("Vertical") * speed * Time.deltaTime;
+        float translationHorizontal = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+        transform.Translate(translationHorizontal, 0, translationVertical);
+
+        float rotation = Input.GetAxis("Horizontal_R") * rotationSpeed * Time.deltaTime;
+        transform.Rotate(0, rotation, 0);
+    }
+}
